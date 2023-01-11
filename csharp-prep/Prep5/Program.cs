cfd1f304f35db724a693f6f70c0ea2c9c38d76c7
Jacob Strong <jacob_strong@byui.edu>
2023-01-11 10:08:33 -0700

    finshed prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        js_DisplayWelcome();
+        string js_name = js_PromptUserName();
+        int js_num = js_PromptUserNumber();
+        js_DisplayResult(js_name, js_num);
+    }
+
+    static void js_DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string js_PromptUserName(){
+        Console.WriteLine("What is your name?");
+        return Console.ReadLine();
+    }
+
+    static int js_PromptUserNumber(){
+        Console.WriteLine("What is your favorite number?");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int js_SquareNumber(int js_num){
+        return js_num*js_num;
+    }
+
+    static void js_DisplayResult(string js_name, int js_num){
+        Console.WriteLine($"{js_name}, the square of your number is {js_SquareNumber(js_num)}");
     }
 }
