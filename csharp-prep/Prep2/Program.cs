23834b3f7c51c12c670d05f567e595a3b449e269
Jacob Strong <jacob_strong@byui.edu>
2023-01-11 09:20:55 -0700

    finished prep3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your percentage grade?");
+        string input1 = Console.ReadLine();
+        int numGrade = int.Parse(input1);
+        string letterGrade = "a";
+
+        if (numGrade >= 90){
+            letterGrade = "A";
+        }
+
+        else if (numGrade >= 80){
+            letterGrade = "B";
+        }
+
+        else if (numGrade >= 70){
+            letterGrade = "C";
+        }
+
+        else if (numGrade >= 60){
+            letterGrade = "D";
+        }
+
+        else {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letterGrade}");
+
+        if (numGrade >= 70){
+            Console.WriteLine("You passed the course :)");
+        }
+
+        else {
+            Console.WriteLine("You failed the course :(");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        string js_input = "yes";
+        do {
+            Console.WriteLine("Guess the magic number!");
+            Random generator = new Random();
+            int js_magic = generator.Next(1, 11);
+            int js_guess = 0;
+            int count = 0;
+            while(js_guess != js_magic){
+                Console.WriteLine("What is your guess?");
+                js_guess = int.Parse(Console.ReadLine());
+                if(js_guess > js_magic){
+                    Console.WriteLine("Lower!");
+                    count++;
+                }
+                else if(js_guess < js_magic){
+                    Console.WriteLine("Higher!");
+                    count++;
+                }
+                else if(js_guess == js_magic){
+                    Console.WriteLine("You win!");
+                    count++;
+                }
+            }
+            Console.WriteLine($"You got it in {count} guesses.");
+            Console.WriteLine("Do you want to play again?");
+            js_input = Console.ReadLine();
+        } while(js_input == "yes");
     }
 }
