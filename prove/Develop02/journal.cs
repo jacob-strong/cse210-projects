f4514f7f0451cbb9f5dea6fd3765e0ec9bd36d8e
Jacob Strong <jacob_strong@byui.edu>
2023-01-28 14:09:31 -0700

    finished journal class

--- prove/Develop02/journal.cs
+++ prove/Develop02/journal.cs
@@ -4,11 +4,18 @@ public class journal{
     }
 
     public void save(){
-        
+        Console.WriteLine("What would you like the file to be named?");
+        string fileName = Console.ReadLine();
+        foreach(string x in entries){
+            File.WriteAllText(fileName, x);
+        }
     }
 
     public void load(){
-
+        Console.WriteLine("What is the name of the file to load?");
+        string fileName = Console.ReadLine();
+        string readEntry = File.ReadAllText(fileName);
+        Console.WriteLine(readEntry);
     }
     public List<string> entries = new List<string>();
 }
