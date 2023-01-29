eb8b0ac93f365ef79ec8844666d89342d1c61937
Jacob Strong <jacob_strong@byui.edu>
2023-01-28 20:41:18 -0700

    finished the whole project

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,19 +2,36 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.Write("Would you like to open your journal? Yes or No");
-        string journal = Console.ReadLine().ToLower();
-        while (journal != "yes"){
+    static void Main(string[] args){
+        Journal journal = new Journal();
+        string choice = "";
+        do{
             Console.WriteLine("Welcome to your journal!");
             Console.WriteLine("1. Write a new entry");
             Console.WriteLine("2. Display a previous entry");
             Console.WriteLine("3. Save all entries to a file");
             Console.WriteLine("4. Load entries from a file");
-            Console.WriteLine("5. Type 'end' to end the program");
+            Console.WriteLine("5. End program");
             Console.WriteLine("What is your choice?");
-            string answer = Console.ReadLine();
-        }
+            choice = Console.ReadLine();
+            if (choice == "1"){
+                journal.addEntry();
+            }
+            else if(choice == "2"){
+                journal.displayEntry();
+            }
+            else if(choice == "3"){
+                journal.saveFile();
+            }
+            else if(choice == "4"){
+                journal.loadFile();
+            }
+            else if(choice == "5"){
+                Console.WriteLine("Have a nice day!");
+            }
+            else{
+                Console.WriteLine("Invalid choice, try again");
+            }
+        } while (choice != "5");
     }
 }
--- prove/Develop02/journal.cs
+++ prove/Develop02/journal.cs
@@ -1,9 +1,20 @@
-public class journal{
-    public void newEntry(string entry){
-        entries.Add(entry);
+public class Journal{
+    public void addEntry(){ 
+        DateTime now = DateTime.Now;
+        Random random = new Random();
+        int randomInt = random.Next(0, 8);
+        string randomPrompt = promptList[randomInt];
+        Console.WriteLine(now);
+        Console.WriteLine(randomPrompt);
+        string entry = Console.ReadLine();
+        entries.Add(now+"\n" + randomPrompt+"\n" + entry);
     }
 
-    public void save(){
+    public void displayEntry(){
+        Console.WriteLine(entries[^1]);
+    }
+
+    public void saveFile(){
         Console.WriteLine("What would you like the file to be named?");
         string fileName = Console.ReadLine();
         foreach(string x in entries){
@@ -11,11 +22,22 @@ public class journal{
         }
     }
 
-    public void load(){
+    public void loadFile(){
         Console.WriteLine("What is the name of the file to load?");
         string fileName = Console.ReadLine();
         string readEntry = File.ReadAllText(fileName);
         Console.WriteLine(readEntry);
     }
     public List<string> entries = new List<string>();
+    public string[] promptList = {
+        "What was the best thing that happened to you today?",
+        "What is one thing you wish you could have done differently?",
+        "Who was your favorite person to talk to today?",
+        "Did I meet anyone interesting today?",
+        "Did I do anything meaningful today?",
+        "Did I meet my personal goals today?",
+        "What was the coolest thing I saw today?",
+        "What was an interesting thought I had today?",
+        "What was something I could’ve done differently?"
+    };
 }
