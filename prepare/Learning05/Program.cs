8d1883386cab5fc3343148c9d4b3e0dc5215c478
Jacob Strong <jacob_strong@byui.edu>
2023-03-01 22:36:23 -0700

    finished Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine("Hello Develop05 World!");
+        Console.WriteLine();
+
+        List<Shape> shapeList = new List<Shape>();
+        Square newSquare = new Square("Blue", 4);
+        shapeList.Add(newSquare);
+        Rectangle newRectangle = new Rectangle("Red", 2, 4);
+        shapeList.Add(newRectangle);
+        Circle newCircle = new Circle("Yellow", 3);
+        shapeList.Add(newCircle);
+
+        foreach(Shape x in shapeList){
+            Console.WriteLine(x.getColor());
+            Console.WriteLine(x.getArea());
+            Console.WriteLine();
+        }
     }
 }
